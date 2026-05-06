3e139f70d0e91b05c3d944ffb8862cd3733d07bf
mu89h82 <kapadiakhyati@johndeere.com>
2026-05-06 18:49:03 +0530

    feat: Added AuthController

---
+++ AuthController/src/test/kotlin/com/example/authcontroller/AuthControllerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.authcontroller
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AuthControllerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
